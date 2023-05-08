54f6673ef2895d13bbc9dd11b27bbe2b8246af7a
DaniOwnerShip <daniorcha@hotmail.com>
2023-05-08 03:15:02 +0200

    up1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xyPlot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Daniel Orcha',
+    author_email='daniorcha@hotmail.com',
+    description='X Y Plot Grahp',
+    keywords='xy plot graph matplotlib',
+    url='https://github.com/DaniOwnerShip/dependencies',
+)
