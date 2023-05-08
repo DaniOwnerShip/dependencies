fe158ed30a1a9976b1e649523566c1f4e8f54605
DaniOwnerShip <daniorcha@hotmail.com>
2023-05-08 03:46:14 +0200

    upd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='xyPlot',
+    name='GraphXY',
     version='0.1',
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib', 'datetime'],
